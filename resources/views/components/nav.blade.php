6dd61993db13c8415988ac4facb183e03ecb7b90
webcolegiolaconcepcion2023 <webcolegiolaconcepcion2023@gmail.com>
2023-02-18 19:26:31 -0500

    Actualización del FQRS

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -27,9 +27,9 @@
             {{-- <li class="nav-item">
                 <a class="nav-link" href="{{ route('allcategories') }}">Cursos</a>
             </li> --}}
-            <li class="nav-item">
+            {{--<li class="nav-item">
                 <a class="nav-link" href="#">FQRS</a>
-            </li>
+            </li>--}}
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">User
                 </a>
